a43e94539f205ef1cd991a292c6ece3b64400e78
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-09-20 14:00:45 +0530

    day2

--- Basics_skills/09tuples&Sets.py
+++ Basics_skills/09tuples&Sets.py
@@ -1,10 +1,45 @@
-#tuples - these are the lists that cannot be changed.
+#tuples - these are the lists that cannot be changed(immutable).
 dimensions = (30,40)#we use parenthesis instead of square brackets
 print(dimensions)
 #we can't change a value in tuples but we can rewrite the tuples..
 dimensions = (40,50)
 for dimension in dimensions:
     print(dimension)
+
+t=(1,2,3,4,5,6,7,8,9)
+#Other than immutability, everything is almost same. we can access elements using index.
+print(t[0])
+print(t[1::2])
+#tuple is somewhat faster than list, should be used when we dont want to change the elements.
+t=(2,) #singleton tuple should be declared using a comma at the end, otherwise it treated as based as int or string or other thing based on the data.
+
+
+#packing and unpacking in tuples
+t=(1,2,3,4) #packing
+#unpacking
+(e1,e2,e3,e4)=t #while unpacking, elements at the left should match the no. of elements in the tuple.
+print(e1)
+print(e2)
+#unpacking and packing can be combined into one statement.
+(e1,e2,e3,e4)=(1,2,3,4)#no. of elements at the left and right should be same.
+
+
+#In tuples, parenthesis can be ignored.
+t=1,2,3,4 #tuple packing
+print(t)
+print(type(t))
+t=1, #singleton tuple
+e1,e2,e3,e4 = 1,2,3,4 #It looks like normal assignment, but it acts like a tuple.
+
+#swap using tuple
+a=1
+b=2
+a,b=b,a #swapped the elements without any need of third variable.
+print(a)
+print(b)
+
+
+
 
 
 # set(it differs from dictionary as dict contains key-values)
---
+++ Intermediate_skills/00exception_handling.py
@@ -0,0 +1,67 @@
+#exception handling :
+#Exception handling - does mean that handling exceptional cases but not syntax errors.
+#python have some built-in exceptions and we can raise custom exceptions as well.
+
+#raising an exception
+x=10
+if x>5:
+	raise Exception('x exceeded 5, x should not exceed 5') #program raises an exception and stop at this line without going further.
+
+
+#AssertionError - Instead fo waiting for the exception, we can assert to check for a statement is true or false and raise an exception if it turns out to be true.
+
+import sys
+assert 'linux' in sys.platform, "this code runs on linux only"
+#the above runs successfully on linux, but if we run it on any other platform we will get an assertion something like this:
+"""
+Traceback (most recent call last):
+  File "<input>", line 2, in <module>
+AssertionError: This code runs on Linux only.
+"""
+
+
+#try and except block 
+#It continue the program inspite of an error.
+"""
+try:
+	run this code
+except:
+	excute this when there is an exception.
+"""
+
+def fun(divide_by):
+	"""divide by function with try and except block"""
+	try:
+		return 40/divide_by
+	except:	#here we used bare except block, but it is not a good practice. It is recommended to catch the exact exception and handle every seperate exception as a seperarte case.
+		return "error,zero divison not allowed"
+print(fun(4))
+print(fun(10))
+print(fun(0))    #zero division is an error,even though program runs to next step without stopping here
+print(fun(3))
+
+def fun(divide_by):
+	"""divide by function with try and except block"""
+	try:
+		return 40/divide_by
+	except ZeroDivisionError as error:	#we handled the exception with the exact exceptional case.
+		return error	#ZeroDivsionError is built-in exception, we can raise custom exceptions too.
+
+
+print(fun(5))
+print(fun(0))
+print(fun(4))
+
+
+#else and finally clauses
+
+"""
+try:
+	run this code.
+except:
+	excute this code when there is an exception,
+else:
+	No exceptions? run this code,
+finally:
+	always run this code.
+"""
